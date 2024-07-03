cf32f336832f31d4fd77c2f6b55e3d9b9e5f0790
Anashkin Aleksei <109337222+NewalexOA@users.noreply.github.com>
2024-07-04 01:20:25 +0300

    add news_details

--- news/views.py
+++ news/views.py
@@ -1,7 +1,11 @@
 # news/views.py
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import News_post
 
 def news(request):
     news = News_post.objects.all()
     return render(request, 'news/news.html', {'news': news})
+
+def news_detail(request, news_id):
+    news_post = get_object_or_404(News_post, id=news_id)
+    return render(request, 'news/news_detail.html', {'news_post': news_post})
